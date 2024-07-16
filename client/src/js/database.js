ca06e44da771042f8307499c7d5b665951a067ea
Derek <sego37@gmail.com>
2024-07-16 16:41:23 -0600

    fixed index.db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ content: content });
+  const request = store.put({ content: content, id:1 });
 
   // Get confirmation of the request.
   const result = await request;
@@ -48,12 +48,12 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.content;
 };
 
 initdb();
